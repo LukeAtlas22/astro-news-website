1754f1101388b281f72e1867afb299ec374485ee
Luca Di Nuzzo <lucadinuzzo22@gmail.com>
2025-09-02 22:42:01 +0200

    Checkpoint (Carousel)

--- src/html/utilities/lists/utils/getSlotLength.ts
+++ src/html/utilities/lists/utils/getSlotLength.ts
@@ -1,5 +1,6 @@
 
 import { parseHTML } from 'linkedom'
+import { wrap } from 'module';
 
 const getSlotLength = function(html: string): number {
   const {document} = parseHTML(html);
@@ -7,4 +8,24 @@ const getSlotLength = function(html: string): number {
   return slotLength;
 }
 
-export { getSlotLength };
+
+type wrapperConfigurator = {
+  elements: Array<Element>,
+  classList?: string,
+  wrapper: 'li' | 'dd',
+  html: unknown,
+};
+
+const wrapper = function(config: wrapperConfigurator): Array<Element> {
+  const {document} = parseHTML(config.html);
+  const wrappedElements = config.elements.map((element: Element) => {
+    const li = document.createElement(`${config.wrapper}`);
+    if (config.classList)
+      li.classList.add(config.classList);
+    li.appendChild(element);
+    return li;
+  })
+  return wrappedElements;
+}
+
+export { getSlotLength, wrapper };
